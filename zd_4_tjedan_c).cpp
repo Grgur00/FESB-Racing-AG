e4af70dfbce0f5ca75bdeb6b66c1d473fe661984
Grgur00 <115583237+Grgur00@users.noreply.github.com>
2023-01-04 19:22:52 +0100

    Update zd_4_tjedan_c).cpp

--- zd_4_tjedan_c).cpp
+++ zd_4_tjedan_c).cpp
@@ -1,10 +1,9 @@
 #include <SPI.h>
 #include <mcp_can.h>
 
-MCP_CAN CAN(10); // Create an MCP_CAN object and specify the slave select pin
+MCP_CAN CAN(10); 
 
 void setup() {
-  // Initialize the MCP_CAN object
   if (CAN.begin(MCP_ANY,CAN_500KBPS,MCP_16MHZ) == CAN_OK) {
     Serial.println("MCP2515 initialized successfully");
   } else {
@@ -13,18 +12,17 @@ void setup() {
 }
 
 void loop() {
-  // Set up the CAN message
-  long unsigned int id = 0x123; // Set the ID of the message
-  byte data[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // Set the data of the message
-  byte length = 8; // Set the length of the message
-  byte flag = 0; // Set the flag for the message (0 for standard message, 1 for extended message)
+ 
+  long unsigned int id = 0x123; 
+  byte data[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  byte length = 8; 
+  byte flag = 0; 
   
-  // Send the message using the MCP_CAN library
   if (CAN.sendMsgBuf(id, flag, length, data) == CAN_OK) {
     Serial.println("Message sent successfully");
   } else {
     Serial.println("Error sending message");
   }
   
-  delay(1000); // Delay for 1 second before sending the next message
+  delay(1000); 
 }
